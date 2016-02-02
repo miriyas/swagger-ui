703eb875ddfaff9938283adaa1c610dc62d66f52
Martin Abelson Sahlen <martin@unacast.com>
2016-02-02 17:33:28 +0100

    Add server and procfile

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var path = require('path');
+var express = require('express');
+var cluster = require('cluster');
+
+var WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+if (cluster.isMaster) {
+
+  for (var i = 0; i < WORKERS; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+} else {
+  var app = express();
+  app.use(express.static(path.join(__dirname, '/dist')));
+  app.listen(8080 || process.env.PORT);
+}
